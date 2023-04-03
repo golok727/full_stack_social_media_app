85beffd5e318969884802aafb1904e20b1c74d5e
Radha743 <worldofnara777@gmail.com>
2023-04-03 11:26:08 +0530

    Post model created

--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Post(models.Model):
+    title= models.CharField(max_length=100, blank=True, null=True)
+    description= models.TextField(blank=True, null=True)
+    image  = models.ImageField(upload_to="post_images", blank=True)
+    user = models.ForeignKey(User, blank=True, null=True,  on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, blank=True, related_name="likes")
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
 
-# Create your models here.
+    def __str__(self): 
+        return self.title
+
+    class Meta: 
+        ordering =  ["-created"]
+    
--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 from datetime import timedelta
 
@@ -39,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+
+    'corsheaders',
     'rest_framework',
+
+    'api',
 ]
 
 SIMPLE_JWT = {
@@ -90,6 +94,8 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -97,6 +103,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+
+    "http://localhost:1573",
+    "http://127.0.0.1:1573",
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -171,3 +185,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Media
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
