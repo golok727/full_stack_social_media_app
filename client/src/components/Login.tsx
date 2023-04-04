b4f9530eee2c1f70209fbb9ecae8c774fe677c3e
Radha743 <worldofnara777@gmail.com>
2023-04-04 16:47:00 +0530

    Login page logic start

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 const Login = () => {
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const userNameRef = useRef<HTMLInputElement>();
+	useEffect(() => {
+		if (userNameRef.current) {
+			userNameRef.current.focus();
+		}
+	}, []);
+
 	return (
 		<div className="min-h-screen text-white flex justify-center items-center ">
 			<div className="  bg-gray-950 bg-opacity-40 rounded px-7 py-4 border-gray-800 border-[1px]">
@@ -23,7 +34,12 @@ const Login = () => {
 							className="bg-transparent border-[1px] border-gray-700 px-3 py-2 rounded text-white placeholder-slate-500"
 							type="text"
 							placeholder="Username"
+							ref={userNameRef as React.MutableRefObject<HTMLInputElement>}
+							value={username}
+							onChange={(e) => setUsername(e.currentTarget.value)}
 							id="username"
+							aria-label="username"
+							required
 							name="username"
 						/>
 					</div>
@@ -40,8 +56,13 @@ const Login = () => {
 						<input
 							className="bg-transparent border-[1px] border-gray-700 px-3 py-2 rounded text-white placeholder-slate-500"
 							type="password"
+							autoComplete="current-password"
 							placeholder="Password"
 							id="password"
+							value={password}
+							onChange={(e) => setPassword(e.currentTarget.value)}
+							aria-label="password"
+							required
 							name="password"
 						/>
 					</div>
@@ -53,6 +74,7 @@ const Login = () => {
 							type="submit"
 							value="Login"
 							className="bg-green-600 cursor-pointer px-3 py-2 rounded font-bold "
+							aria-label="submit"
 						/>
 					</div>
 				</form>
