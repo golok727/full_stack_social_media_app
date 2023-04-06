af41a47f803a72d5b7dd3aee33242293cba33ada
Radha743 <worldofnara777@gmail.com>
2023-04-06 14:47:32 +0530

    custom login

--- server/api/urls.py
+++ server/api/urls.py
@@ -5,10 +5,10 @@ from .serializers import MyTokenObtainPairView
 
 urlpatterns = [
     path("", views.getRoutes),
-    path("auth/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("auth/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/login/", views.loginUser, name="login_view"),
     path("auth/logout/", views.logoutUser, name="logout_view"),
-    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_pair_view"),
+    path("auth/token/refresh/", views.refreshTokens, name="token_pair_view"),
     path("posts/", views.getPosts, name="get_posts")
 ]
 
